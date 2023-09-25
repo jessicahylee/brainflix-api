34b895e14c0895048dc2f54e31102a006df17137
jessicahylee <jehyerinn@gmail.com>
2023-09-25 17:22:28 -0400

    FINAL PROJECT

--- Routes/videoRoutes.js
+++ Routes/videoRoutes.js
@@ -8,34 +8,26 @@ const cors = require('cors')
 
 router.use(cors()) // Browser has access.
 router.use(express.json())
-
+// console.log('hello!', __dirname)
 const videosDetailed = JSON.parse(
   fs.readFileSync(path.join(__dirname, '..', 'data', 'video-details.json')),
-)
-const videos = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '..', 'data', 'video.json')),
 )
 
 //Get a list of videos
 router.get('/', (req, res) => {
-  console.log('videos are here')
+  // console.log('videos are here')
   res.status(200).json(videosDetailed)
 })
 
 router.get('/video/:id', (req, res) => {
-  fs.readFile(
-    path.join(__dirname, 'files', 'video-details.json'),
-    (err, fileData) => {
-      if (err) {
-        console.error(err)
-        res.status(500).send('Error reading file')
-      } else {
-        console.log(fileData)
-        console.log('videos are uploaded')
-        res.json(videosDetailed)
-      }
-    },
-  )
+  // Get video with id
+  // Fetch video with the from
+  const id = req.params.id
+  console.log(id)
+  const video = videosDetailed.filter((v) => v.id === id)
+  console.log(video)
+  res.json(video)
+
 })
 
 router.post('/', (req, res) => {
